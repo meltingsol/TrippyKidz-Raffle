2d56ebda9dbde4cde1d03c9da02f0bbe5a27da42
meltingsol <102722326+meltingsol@users.noreply.github.com>
2022-07-15 17:18:59 +0530

    Update AirdropPage.js

--- src/dashboard/components/AirdropPage.js
+++ src/dashboard/components/AirdropPage.js
@@ -501,6 +501,10 @@ const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
                  Total Tickets bought: {numEntries} 
                 </div>
 
+                <div>
+                  <iframe src="https://free.timeanddate.com/countdown/i8ez6mfk/cf12/cm0/cu4/ct0/cs0/ca0/co1/cr0/ss0/cac000/cpcfff/pcfff/tcfff/fs100/szw320/szh135/tatRAFFLE%20ENDS%20IN/tac000/tptRAFFLE%20ENDED/tpc000/iso2022-07-18T00:00:00" allowtransparency="true" frameborder="0" width="148" height="51"></iframe>
+                </div>
+
 
                 <div className="dark:bg-slate-800" style={{width:"100%",textAlign:"center"}}>
               <button className="button-26" role="button" onClick={handleDecrementRaffle}>-</button>
